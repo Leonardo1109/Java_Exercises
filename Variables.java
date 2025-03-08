b6b4fcf13dc4178ee3e662fa7342703eb9fa6582
Leonardo1109 <leonardoolveram11@gmail.com>
2025-03-07 22:03:56 -0600

    Corrección de errores en Variables.java

--- Variables.java
+++ Variables.java
@@ -1,3 +1,20 @@
 public class Variables {
-    
+    /* Java nos permite usar variables asi que crearemos algunas para poder usarlas */
+
+    /* Declaramos las variables */
+    public int edad;
+    public String nombre;
+    public float estatura;
+    public boolean activo;
+
+    public static void main(String[] args) {
+        Variables persona = new Variables();
+        persona.edad = 21;
+        persona.nombre = "Leonardo";
+        persona.estatura = 1.70f;
+        persona.activo = false;
+        System.out.printf("%d %s %.2f %b%n", persona.edad, persona.nombre, persona.estatura,
+            persona.activo);
+    }
+
 }
