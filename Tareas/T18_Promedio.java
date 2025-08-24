c3005802377d925f72651d88258214214f22c3a4
Leonardo1109 <leonardoolveram11@gmail.com>
2025-08-23 20:19:01 -0600

    Commit 23-08-21_T18

---
+++ Tareas/T18_Promedio.java
@@ -0,0 +1,11 @@
+package Tareas;
+
+public class T18_Promedio {
+    public static void main(String[] args) {
+        int[] calificaciones = {10, 9, 8, 10, 9 , 10, 10};
+        
+        for (int i = 0; i < calificaciones.length; i++) {
+            System.out.printf("Calificacion %d: %d%n", i + 1, calificaciones[i]);
+        }
+    }
+}
