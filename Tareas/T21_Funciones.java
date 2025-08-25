cbf6c270c03ff6587982dfb84133e93d9e06779b
Leonardo1109 <leonardoolveram11@gmail.com>
2025-08-24 22:02:31 -0600

    Commit 25-08-24_T21_ver2

--- Tareas/T21_Funciones.java
+++ Tareas/T21_Funciones.java
@@ -6,6 +6,8 @@ public class T21_Funciones {
         System.out.println(sumar(10, 15));
         par(10);
         par(5);
+        System.out.println(sumaRecursiva(5));
+        cadena("Hola mundo");
     }
 
     private static void imprimir(String texto){
@@ -20,5 +22,20 @@ public class T21_Funciones {
     private static void par (int numero){
         boolean esPar = numero % 2 == 0 ? true : false;
         System.out.println(esPar == true ? numero + " es par" : numero + " no es par");
+    }
+
+    private static int sumaRecursiva (int numeroFinal){
+        if (numeroFinal <= 0) {
+            return 0;
+        }else{
+            return numeroFinal + sumaRecursiva(numeroFinal - 1);
+        }
+    }
+
+    private static void cadena (String cadena){
+        System.out.printf("%s tiene %d caracteres%n", cadena, cadena.length());
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.printf("%s ", cadena.charAt(i));
+        }
     }
 }
